febe6206c0c02efd545dfae090b41d6e75f38b1e
Marc <marclang123@hotmail.com>
2021-02-04 09:38:44 +0100

    add Joy feature

--- exemple_1.py
+++ exemple_1.py
@@ -5,5 +5,8 @@ Created on 06/4/2020
 @author: Marc LANG
 """
 
-def hello_world():
-    print("hello world")
+def hello_world(joy=False):
+    if not joy:
+        print("hello world")
+    else:
+        print("hello world !!  =)")
