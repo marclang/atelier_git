cec8c912fd4f8cd0bfc123e9febf56c58c0c8693
Marc <marclang123@hotmail.com>
2021-02-04 09:40:30 +0100

    add happiness feature

--- exemple_1.py
+++ exemple_1.py
@@ -5,5 +5,8 @@ Created on 06/4/2020
 @author: Marc LANG
 """
 
-def hello_world():
-    print("hello world")
+def hello_world(happiness=False):
+    if not happiness:
+        print("hello world")
+    else:
+        print("hello world !!  =)")
